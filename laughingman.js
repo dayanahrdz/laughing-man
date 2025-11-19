cc29631d07eec51ebadb1b196c30c598bfc7d40b
Dayana Hernandez <dayanahern15@gmail.com>
2025-11-19 10:14:51 -0500

    fixed alert

--- laughingman.js
+++ laughingman.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded',function(){
-    let squareElement = document.getElementById("square");
-    squareElement.addEventListener("click". ()=>{
-        alert("OMG YOU CAUGHT ME!");
-    });
+document.addEventListener('DOMContentLoaded', ()=>{
+let squareElement = document.getElementById("square");
+//let squareElement = document. querySelector ("#square");
+squareELement.addEventListener("click", ()=>{
+alert ("OMG YOU CAUGHT ME!");
 });
+9
+}):
