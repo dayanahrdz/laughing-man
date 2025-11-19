e9d6f87be30df3ac983b643df39d587834e0a176
Dayana Hernandez <dayanahern15@gmail.com>
2025-11-19 15:43:49 -0500

    edits

--- laughingman.js
+++ laughingman.js
@@ -1,8 +1,31 @@
+var SQUARE_COUNT = 3;
+var SPEED = 5;
+var TIMER_SPEED = 100;
+
 document.addEventListener('DOMContentLoaded', ()=>{
 let squareElement = document.getElementById("square");
 //let squareElement = document. querySelector ("#square");
 squareELement.addEventListener("click", ()=>{
 alert ("OMG YOU CAUGHT ME!");
 });
-9
-}):
+
+
+let box = document.querySelector("box");
+
+for (let i=0 ; i<SQUARE_COUNT; i++)
+{
+    //create the square
+    let square = document.createElement('img');
+    square.src = 'laughing_man.jpg';
+    square.alt = 'Catch the Laughing Man!';
+    square.className = 'square';
+    box.appendChild(square);
+    //add it to the screen
+}
+    let allSquares = box.children;
+    let squareArray = Array.from(children);
+    squareArray.forEach(function(element,index){
+        var dx = SPEED * (Math.random)
+    })
+
+});
